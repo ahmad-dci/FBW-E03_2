d3cb167327a8f59641fbf870d121c1477e6ffe26
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-11-05 14:33:27 +0100

    contact us send data

--- 2-book-store/app.js
+++ 2-book-store/app.js
@@ -4,6 +4,9 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
+// setup ejs view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // set public folder 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -11,15 +14,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     //console.log(__dirname);
     // res.sendFile(__dirname + '/views/index.html');
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    //res.sendFile(path.join(__dirname, 'views', 'index.html'));
     // console.log(req);
+    res.render('index');
 })
 
 app.get('/contactus', (req, res) => {
     //console.log(__dirname);
     // res.sendFile(__dirname + '/views/index.html');
-    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+    // res.sendFile(path.join(__dirname, 'views', 'contact.html'));
     // console.log(req);
+    res.render('contact');
 })
 // nodemon app.js
 
