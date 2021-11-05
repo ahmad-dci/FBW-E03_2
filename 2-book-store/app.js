238909bc4ea6ca51ec7696739516cfc450ce085b
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-11-05 15:16:08 +0100

    success send contact data

--- 2-book-store/app.js
+++ 2-book-store/app.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// set up the handler of posted data
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 // set public folder 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,7 +31,10 @@ app.get('/contactus', (req, res) => {
     res.render('contact');
 })
 // nodemon app.js
-
+// get post data from contact us form
+app.post('/contactus', (req, res) => {
+    console.log(req.body);
+})
 
 app.listen(port, () => {
   console.log('Server is up and running on port number ' + port);
