a8aeae29f3323861cc8c59dc43c5faa9ff461bc7
ahmad-teaching <ahmad.osman@digitalcareerinstitute.org>
2021-12-10 15:20:19 +0100

    setup router

--- 3-webshop/src/index.js
+++ 3-webshop/src/index.js
@@ -4,17 +4,31 @@ import Results from './components/results'
 import Search from './components/search'
 import { StorageProvider } from './contexts'
 import './app.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+function Main() {
+    return <div className="container">
+        <Search />
+        <Results />
+    </div>
+}
+function Contact() {
+    return <div>
+        <h2>this is contact page</h2>
+        <Link to="/">home</Link>
+        </div>
+}
 function App() {
     return (
         <StorageProvider>
-            <div className="container">
-                <Search />
-                <Results />
-            </div>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </Router>
         </StorageProvider>
     )
 }
-
 ReactDOM.render(<App />, document.getElementById('root'))
 
 
